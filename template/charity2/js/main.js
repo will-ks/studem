f3a46c879af05d1120cc90068d4d10d2e19703b6
Will Schaller <willschaller@gmail.com>
2019-09-07 20:38:43 +0200

    Set up Prettier

--- template/charity2/js/main.js
+++ template/charity2/js/main.js
@@ -1,35 +1,40 @@
 (function($) {
-  "use strict"
+  "use strict";
 
   // Mobile Nav button toggle
-  $('.navbar-toggle-btn').on('click', function() {
-    $('.navbar-menu').toggleClass('navbar-menu-active');
+  $(".navbar-toggle-btn").on("click", function() {
+    $(".navbar-menu").toggleClass("navbar-menu-active");
   });
 
   // Mobile Search button toggle
-  $('.search-toggle-btn').on('click', function() {
-    $('.navbar-search').toggleClass('navbar-search-active');
+  $(".search-toggle-btn").on("click", function() {
+    $(".navbar-search").toggleClass("navbar-search-active");
   });
 
   // Mobile dropdown
-  $('.navbar-menu .has-dropdown > a').on('click', function(e) {
+  $(".navbar-menu .has-dropdown > a").on("click", function(e) {
     e.preventDefault();
-    $(this).parent().toggleClass('dropdown-active');
+    $(this)
+      .parent()
+      .toggleClass("dropdown-active");
   });
 
   // Home Owl
-  $('#home-owl').owlCarousel({
+  $("#home-owl").owlCarousel({
     items: 1,
     loop: true,
     autoplay: true,
     margin: 0,
     nav: true,
     dots: true,
-    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ]
   });
 
   // Testimonial Owl
-  $('#testimonial-owl').owlCarousel({
+  $("#testimonial-owl").owlCarousel({
     loop: true,
     margin: 15,
     dots: true,
@@ -45,10 +50,8 @@
     }
   });
 
-	// Parallax Background
-	$.stellar({
-		responsive: true
-	});
-
-
+  // Parallax Background
+  $.stellar({
+    responsive: true
+  });
 })(jQuery);
