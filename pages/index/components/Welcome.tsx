87ecdeaf3bbc2439d8e05af4d7e5f57930a04b0e
Will Schaller <willschaller@gmail.com>
2019-09-09 22:05:56 +0200

    More components

--- pages/index/components/Welcome.tsx
+++ pages/index/components/Welcome.tsx
@@ -5,18 +5,18 @@ const Welcome = () => (
         <div className="col-12 col-lg-6 order-2 order-lg-1">
           <div className="welcome-content">
             <header className="entry-header">
-              <h2 className="entry-title">Wellcome to our Charity</h2>
+              <h2 className="entry-title">Welcome to STUDEM</h2>
             </header>
 
             <div className="entry-content mt-5">
               <p>
+                STUDEM is a ... Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Mauris tempus vestib ulum mauris quis aliquam.
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-                tempus vestib ulum mauris quis aliquam. Lorem ipsum dolor sit
-                amet, consectetur adipiscing elit. Mauris tempus vestibulum
-                mauris quis aliquam. Integer accumsan sodales odio, id tempus
-                velit ullamcorper id. Quisque at erat eu libero consequat
-                tempus. Quisque molestie convallis tempus. Ut semper purus
-                metus, a euismod sapien sodales ac. Duis viverra eleifend
+                tempus vestibulum mauris quis aliquam. Integer accumsan sodales
+                odio, id tempus velit ullamcorper id. Quisque at erat eu libero
+                consequat tempus. Quisque molestie convallis tempus. Ut semper
+                purus metus, a euismod sapien sodales ac. Duis viverra eleifend
                 fermentum.
               </p>
             </div>
@@ -30,7 +30,7 @@ const Welcome = () => (
         </div>
 
         <div className="col-12 col-lg-6 mt-4 order-1 order-lg-2">
-          <img src="../../static/images/welcome.jpg" alt="welcome" />
+          <img src="../../static/images/welcome.jpg" alt="Welcome to STUDEM" />
         </div>
       </div>
     </div>
